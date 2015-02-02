6d745c142f01b53cb0904a99deec251c0a585bec
Christopher Lichti <chris.lichti@gmail.com>
2015-02-01 20:57:10 -0800

    Added roles to users and now creating a default admin user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+  enum role: [:user, :admin]
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :lockable, :confirmable, :timeoutable, :omniauthable
+
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
 end
