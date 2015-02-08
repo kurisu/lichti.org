d3a94fd02f76e107b319f9904939e9e00d7781f5
Christopher Lichti <chris.lichti@gmail.com>
2015-02-08 14:58:59 -0800

    Book purchase links are now buttons in the appropriate places, with a default amazon search url when no direct purchase url was provided.

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,6 @@
 module BooksHelper
+
+  def amazon_url_for(book)
+    "http://www.amazon.com/gp/search/ref=sr_adv_b/?search-alias=stripbooks&unfiltered=1&field-keywords=&field-author=#{book.author}&field-title=#{book.title}&field-isbn=#{book.isbn}&field-publisher=&node=&field-p_n_condition-type=&field-feature_browse-bin=&field-subject=&field-language=&field-dateop=During&field-datemod=&field-dateyear=&sort=relevanceexprank&Adv-Srch-Books-Submit.x=0&Adv-Srch-Books-Submit.y=0"
+  end
 end
